53089326e1b94a10fda99b7aae82101443c8f0c3
Scherer <fsvicentin@hotmail.com>
2019-06-01 00:19:04 -0300

    ve se ta bom

--- apCaminhosMarte/apCaminhosMarte/Form1.cs
+++ apCaminhosMarte/apCaminhosMarte/Form1.cs
@@ -20,6 +20,7 @@ namespace apCaminhosMarte
         ArvoreBinaria<Cidade> arvore;
         int qtdCidades;
         Grafo grafo;
+        Bitmap gif;
 
         Color corNo = Color.Blue;
         Color corLinhaArvore = Color.Red;
@@ -88,6 +89,8 @@ namespace apCaminhosMarte
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            gif = new Bitmap("../../../../Imagens/cidadeDestino.gif");
+            ImageAnimator.Animate(gif, drawFrame);
             arvore = new ArvoreBinaria<Cidade>();
             var leitorCidades = new StreamReader("cidades.txt", Encoding.UTF7);           
             while (!leitorCidades.EndOfStream)
@@ -139,8 +142,10 @@ namespace apCaminhosMarte
                     if (dist != 0)
                         DesenhaLinha(l,c,dist,gfx);
                 }
+            int origem = lsbOrigem.SelectedIndex;
+            int destino = lsbDestino.SelectedIndex;
             arvore.InOrdem((Cidade c) => {
-                DesenhaCidade(c, gfx);
+                DesenhaCidade(c, gfx, c.Codigo == origem, c.Codigo == destino);
             });
         }
 
@@ -173,12 +178,30 @@ namespace apCaminhosMarte
             }
         }
 
-        private void DesenhaCidade(Cidade c, Graphics gfx)
+        private void DesenhaCidade(Cidade c, Graphics gfx, bool origem, bool destino)
         {
             int x = pbMapa.Size.Width * c.X / 4096 - DIAMETRO_CIDADE / 2;
             int y = pbMapa.Size.Height * c.Y / 2048 - DIAMETRO_CIDADE / 2;
             gfx.FillEllipse(new SolidBrush(corCidade), x , y , DIAMETRO_CIDADE, DIAMETRO_CIDADE);
-            gfx.DrawString(c.Nome, new Font("Century Gothic", 10, FontStyle.Bold), new SolidBrush(corCidade), new Point(x - 10, y + 10));         
+            gfx.DrawString(c.Nome, new Font("Century Gothic", 10, FontStyle.Bold), new SolidBrush(corCidade), new Point(x - 10, y + 10));
+            if (origem)
+                gfx.DrawImage(Image.FromFile("../../../../Imagens/cidadeOrigem.png"), x - DIAMETRO_CIDADE * 1.5f, y - DIAMETRO_CIDADE * 3.5f, DIAMETRO_CIDADE * 4, DIAMETRO_CIDADE * 4);
+            if (destino)
+            {
+                ImageAnimator.UpdateFrames(gif);
+                using (Bitmap bmp = new Bitmap(gif.Width, gif.Height))
+                {
+                    using (Graphics gr = Graphics.FromImage(bmp))
+                    {
+                        gr.DrawImage(gif, 0, 0);
+                    }
+                    gfx.DrawImage(bmp, x - DIAMETRO_CIDADE * 2.5f, y - DIAMETRO_CIDADE * 2.5f, DIAMETRO_CIDADE * 6, DIAMETRO_CIDADE * 6);
+                }
+            }
+        }
+        private void drawFrame(object sender, EventArgs e)
+        {
+            pbMapa.Invalidate();
         }
 
         private void tpArvore_Paint(object sender, PaintEventArgs e)
